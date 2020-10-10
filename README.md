174b1a95bf9c01e01c2e1c137f523bfbf55c8d38
Harsimran Virk <harsimranvirk99@gmail.com>
2020-10-10 12:55:08 +0530

    Added code in length.py, added yapf formatter

--- README.md
+++ README.md
@@ -1,1 +1,7 @@
 ### Open-Source-Workshop
+
+##### Basic usage of this project
+Run `python length.py --inch-to-cm 5` to convert 5 inch to cm.
+
+##### yapf formatter
+Run `python format.py` to auto-format files using yapf formatter
---
+++ length.py
@@ -0,0 +1,37 @@
+import argparse
+
+parser = argparse.ArgumentParser()
+parser.add_argument('--inch-to-cm', type=float, dest="inch_to_cm")
+parser.add_argument('--cm-to-inch', type=float, dest="cm_to_inch")
+args = parser.parse_args()
+
+
+def main():
+    inch_to_cm_helper()
+    cm_to_inch_helper()
+
+
+# Helper functions to check if the arg exists or not
+def inch_to_cm_helper():
+    inch = args.inch_to_cm
+    if (inch):
+        inch_to_cm(inch)
+
+
+def cm_to_inch_helper():
+    cm = args.cm_to_inch
+    if (cm):
+        cm_to_inch(cm)
+
+
+# Converter functions
+def inch_to_cm(inch):
+    print(f'{inch} inch in cm is: {inch*2.54}cm')
+
+
+def cm_to_inch(cm):
+    print(f'{cm}cm in inch is: {cm/2.54} inch')
+
+
+if (__name__ == "__main__"):
+    main()
