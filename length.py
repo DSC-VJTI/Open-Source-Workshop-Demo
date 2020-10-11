08afa0245576c49c8dbb872cf804ab7d358e4b65
Vriddhigupta <vriddhigupta898@gmail.com>
2020-10-11 13:56:49 +0530

    changes

--- length.py
+++ length.py
@@ -17,6 +17,19 @@ def inch_to_cm_helper():
     if inch:
         inch_to_cm(inch)
 
+# args parser for cm to mm converter
+parser.add_argument('--cm-to-mm', type=float, dest="cm_to_mm")
+
+# helper function for checking args for cm_to_mm function
+def cm_to_mm_helper():
+    cm_mm = args.cm_to_mm
+    if (cm_mm):
+        cm_to_mm(cm_mm)
+
+# function to convert cm to mm
+def cm_to_mm(cm_mm):
+    print(f'{cm_mm}cm in mm is: {cm*10} mm')
+
 
 def cm_to_inch_helper():
     cm = args.cm_to_inch
