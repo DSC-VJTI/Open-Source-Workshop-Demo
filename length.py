20944ce03a48a9e2c71daf276872fc9cc498bc3d
Reshmika Sreenath Nambiar <reshmikasnambiar@gmail.com>
2020-10-11 13:51:39 +0530

    cm to mm added

--- length.py
+++ length.py
@@ -4,7 +4,8 @@ parser = argparse.ArgumentParser()
 parser.add_argument('--inch-to-cm', type=float, dest="inch_to_cm")
 parser.add_argument('--cm-to-inch', type=float, dest="cm_to_inch")
 args = parser.parse_args()
-
+# args parser for cm to mm converter
+parser.add_argument('--cm-to-mm', type=float, dest="cm_to_mm")
 
 def main():
     inch_to_cm_helper()
@@ -17,6 +18,15 @@ def inch_to_cm_helper():
     if inch:
         inch_to_cm(inch)
 
+# helper function for checking args for cm_to_mm function
+def cm_to_mm_helper():
+    cm_mm = args.cm_to_mm
+    if (cm_mm):
+        cm_to_mm(cm_mm)
+
+# function to convert cm to mm
+def cm_to_mm(cm_mm):
+    print(f'{cm_mm}cm in mm is: {cm*10} mm')
 
 def cm_to_inch_helper():
     cm = args.cm_to_inch
