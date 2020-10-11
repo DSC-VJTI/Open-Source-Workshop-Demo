ba18f61407b129d104d5b2c98ae758e7ed3bdd32
Mahek Ajay Salia <saliamahek13@gmail.com>
2020-10-11 12:22:59 +0400

    Learning to pull

--- length.py
+++ length.py
@@ -23,6 +23,19 @@ def cm_to_inch_helper():
     if (cm):
         cm_to_inch(cm)
 
+# args parser for cm to mm converter
+parser.add_argument('--cm-to-mm', type=float, dest="cm_to_mm")
+
+# helper function for checking args for cm_to_mm function
+def cm_to_mm_helper():
+    cm_mm = args.cm_to_mm
+    if (cm_mm):
+        cm_to_mm(cm_mm)
+
+# function to convert cm to mm
+def cm_to_mm(cm_mm):
+    print(f'{cm_mm}cm in mm is: {cm*10} mm')
+
 
 # Converter functions
 def inch_to_cm(inch):
