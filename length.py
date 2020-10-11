ac506374a8c8aec1d743019e5e665f1eeb035f17
MMAP <mehdi.patel@gmail.com>
2020-10-11 13:50:55 +0530

    pushing to forked repo. added cm to mm converter

--- length.py
+++ length.py
@@ -3,13 +3,21 @@ import argparse
 parser = argparse.ArgumentParser()
 parser.add_argument('--inch-to-cm', type=float, dest="inch_to_cm")
 parser.add_argument('--cm-to-inch', type=float, dest="cm_to_inch")
+# args parser for cm to mm converter
+parser.add_argument('--cm-to-mm', type=float, dest="cm_to_mm")
 args = parser.parse_args()
 
 
 def main():
     inch_to_cm_helper()
     cm_to_inch_helper()
+    cm_to_mm_helper() 
 
+# helper function for checking args for cm_to_mm function
+def cm_to_mm_helper():
+    cm_mm = args.cm_to_mm
+    if (cm_mm):
+        cm_to_mm(cm_mm)
 
 # Helper functions to check if the arg exists or not
 def inch_to_cm_helper():
@@ -31,6 +39,10 @@ def inch_to_cm(inch):
 
 def cm_to_inch(cm):
     print(f'{cm}cm in inch is: {cm/2.54} inch')
+
+# function to convert cm to mm
+def cm_to_mm(cm_mm):
+    print(f'{cm_mm}cm in mm is: {cm_mm*10} mm')
 
 
 if (__name__ == "__main__"):
