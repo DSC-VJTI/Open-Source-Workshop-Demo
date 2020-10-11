1297c32ebe3e79b10582e5b63c46aab1d196b6c1
meshtag <prathameshtagore@gmail.com>
2020-10-11 13:51:33 +0530

    check

--- length.py
+++ length.py
@@ -35,3 +35,18 @@ def cm_to_inch(cm):
 
 if __name__ == "__main__":
     main()
+
+
+
+# # args parser for cm to mm converter
+# parser.add_argument('--cm-to-mm', type=float, dest="cm_to_mm")
+
+# # helper function for checking args for cm_to_mm function
+# def cm_to_mm_helper():
+#     cm_mm = args.cm_to_mm
+#     if (cm_mm):
+#         cm_to_mm(cm_mm)
+
+# # function to convert cm to mm
+# def cm_to_mm(cm_mm):
+#     print(f'{cm_mm}cm in mm is: {cm*10} mm')
