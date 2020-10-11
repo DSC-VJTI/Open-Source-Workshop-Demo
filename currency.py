91ea14a46fd8f1eb94419c0704e07a0479cc0a47
Tushar <tushar_bauskar12@outlook.com>
2020-10-11 13:56:59 +0530

    JPY to INR and USD to INR currency conversion

---
+++ currency.py
@@ -0,0 +1,8 @@
+# USD to INR convertor
+def usd_to_inr(usd):
+    print(f'{usd} USD is equal to {72.96 * usd} INR')
+
+
+# JPY to INR convertor
+def jpy_to_inr(jpy):
+    print(f'{jpy} JPY is equal to {0.69 * jpy} INR')
